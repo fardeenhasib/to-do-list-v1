623ad0378fd9a5416633d7d5566cbbdb76146b7d
fardeenhasib <fardeenmozumder@gmail.com>
2023-05-04 12:08:40 -0500

    todays work list added

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let listItems = ["Code", "Eat", "Sleep"];
+let workItems = [];
+
 app.get("/", function (req, res) {
 
     var today = new Date();
@@ -14,6 +17,24 @@ app.get("/", function (req, res) {
     let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", { todoDay: day, todoList: listItems });
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { todoDay: "Work-List", todoList: workItems })
+});
+
+app.post("/", function (req, res) {
+    console.log(req.body);
+    let newItem = req.body.NewItem;
+    let listType = req.body.ListType;
+    if (listType === "Work-List") {
+        workItems.push(newItem);
+        res.redirect("/work");
+    } else {
+        listItems.push(newItem);
+        res.redirect("/");
+    }
+
 });
 
 app.listen(3000);
